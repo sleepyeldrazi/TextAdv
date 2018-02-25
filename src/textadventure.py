711ed0a2b365f0eb106ac60304458fa790a812ef
sleepyeldrazi <konik98@gmail.com>
2018-02-26 00:13:13 +0100

    some cleaning

--- src/textadventure.py
+++ src/textadventure.py
@@ -1,4 +1,4 @@
-map = [
+gameMap = [
     [0, 0, 0, 0, 0, 0],
     [0, 1, 1, 0, 0, 0],
     [0, 0, 1, 10, 13, 0],
@@ -13,46 +13,46 @@ class Player:
     oldLoc = {"x": 1, "y": 1}
 
 
-def look_for_path(mp, loc):
-    if mp[loc["y"] + 1][loc["x"]] != 0:
+def look_for_path(current_map, loc):
+    if current_map[loc["y"] + 1][loc["x"]] != 0:
         print("You can go south")
-    if mp[loc["y"] - 1][loc["x"]] != 0:
+    if current_map[loc["y"] - 1][loc["x"]] != 0:
         print("You can go north")
-    if mp[loc["y"]][loc["x"] + 1] != 0:
+    if current_map[loc["y"]][loc["x"] + 1] != 0:
         print("You can go east")
-    if mp[loc["y"]][loc["x"] - 1] != 0:
+    if current_map[loc["y"]][loc["x"] - 1] != 0:
         print("You can go west")
 
 
-def map_info(mp, pl):
-    if mp[pl.location["y"]][pl.location["x"]] == 1:
+def map_info(current_map, pl):
+    if current_map[pl.location["y"]][pl.location["x"]] == 1:
         print("Forest")
-    elif mp[pl.location["y"]][pl.location["x"]] == 0:
+    elif current_map[pl.location["y"]][pl.location["x"]] == 0:
         print("You can't go there")
         pl.location = pl.oldLoc
-    elif mp[pl.location["y"]][pl.location["x"]] == 10:
+    elif current_map[pl.location["y"]][pl.location["x"]] == 10:
         print("Crossroad")
-    elif mp[pl.location["y"]][pl.location["x"]] == 11:
+    elif current_map[pl.location["y"]][pl.location["x"]] == 11:
         print("Farm")
-    elif mp[pl.location["y"]][pl.location["x"]] == 13:
+    elif current_map[pl.location["y"]][pl.location["x"]] == 13:
         print("Dead end")
-    elif mp[pl.location["y"]][pl.location["x"]] == 2:
+    elif current_map[pl.location["y"]][pl.location["x"]] == 2:
         print("Cave entrance")
+
 
 def action(command, pl):
     available_actions = "take, move, hit, look around, look at, go"
-    compass = "north, east, south, west, n, e, s, w"
     entered_command = command.split()
     if entered_command[0] in available_actions:
         if "go" in command:
             pl.oldLoc = pl.location
-            if "n" == entered_command[1][0]:
+            if entered_command[1][0] == "n":
                 pl.location["y"] = pl.location["y"] - 1
-            elif "s" == entered_command[1][0]:
+            elif entered_command[1][0] == "s":
                 pl.location["y"] = pl.location["y"] + 1
-            elif "e" == entered_command[1][0]:
+            elif entered_command[1][0] == "e":
                 pl.location["x"] = pl.location["x"] + 1
-            elif "w" == entered_command[1][0]:
+            elif entered_command[1][0] == "w":
                 pl.location["x"] = pl.location["x"] - 1
             else:
                 print("I don't understand")
@@ -61,7 +61,7 @@ def action(command, pl):
 playerOne = Player
 
 while True:
-    #print(playerOne.location)
-    map_info(map, playerOne)
-    look_for_path(map, playerOne.location)
+    # print(playerOne.location)
+    map_info(gameMap, playerOne)
+    look_for_path(gameMap, playerOne.location)
     action(input("> "), playerOne)
